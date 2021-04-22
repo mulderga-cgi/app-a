faab2dbd71dca36b72d9bd4e1008266ee2fef4dc
Gerard Mulder <g.mulder@cgi.com>
2021-04-22 11:17:15 +0200

    feat: working simple controller
    Signed-off-by: Gerard Mulder <g.mulder@cgi.com>

--- src/main/java/com/cgi/appa/AppAApplication.java
+++ src/main/java/com/cgi/appa/AppAApplication.java
@@ -1,13 +1,28 @@
 package com.cgi.appa;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AppAApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppAApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppAApplication.class, args);
+    }
 
+}
+
+@RestController
+class HelloWorld {
+
+    @Value("${my.app.name}")
+    private String MY_APP_NAME;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world! From: " + MY_APP_NAME;
+    }
 }
